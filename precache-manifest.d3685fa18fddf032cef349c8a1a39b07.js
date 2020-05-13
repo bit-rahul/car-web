b099c00713ea56ca30fd3856b98e725db2cc60c5
rahulsups <rahul.27bok@gmail.com>
2020-05-14 01:01:57 +0530

    Updates

---
+++ precache-manifest.d3685fa18fddf032cef349c8a1a39b07.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0a0803a90eb9136404395b64e5259a",
+    "url": "/car-web/index.html"
+  },
+  {
+    "revision": "7f17c5ab9896d976282f",
+    "url": "/car-web/static/css/2.ba3816ef.chunk.css"
+  },
+  {
+    "revision": "8727fa6074a255aa2d00",
+    "url": "/car-web/static/css/main.bc9edf36.chunk.css"
+  },
+  {
+    "revision": "7f17c5ab9896d976282f",
+    "url": "/car-web/static/js/2.f4241556.chunk.js"
+  },
+  {
+    "revision": "e303234b7c98ab07140fda539d130e8b",
+    "url": "/car-web/static/js/2.f4241556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8727fa6074a255aa2d00",
+    "url": "/car-web/static/js/main.9732c8d5.chunk.js"
+  },
+  {
+    "revision": "02af34caaeac11eac4ea",
+    "url": "/car-web/static/js/runtime-main.a397080b.js"
+  },
+  {
+    "revision": "95db07da44a00e838e07bddb2038627b",
+    "url": "/car-web/static/media/bg1.95db07da.JPG"
+  },
+  {
+    "revision": "212504fa9ae7ef298f83b897df8acaf1",
+    "url": "/car-web/static/media/car.212504fa.png"
+  },
+  {
+    "revision": "6f89e41ed4ce8fa525ba5b99954b8d3a",
+    "url": "/car-web/static/media/demo.6f89e41e.jpg"
+  },
+  {
+    "revision": "b39ac878276446ceb0cf5d77cb989c57",
+    "url": "/car-web/static/media/demo2.b39ac878.jpg"
+  },
+  {
+    "revision": "aebfaf1c1bf764c37dc68abdeee514f0",
+    "url": "/car-web/static/media/demo3.aebfaf1c.png"
+  },
+  {
+    "revision": "55f9885e265d168e93db1fc3f6f18167",
+    "url": "/car-web/static/media/ford.55f9885e.JPG"
+  },
+  {
+    "revision": "8b25a976da62435bcb43194051a9993f",
+    "url": "/car-web/static/media/honda.8b25a976.JPG"
+  },
+  {
+    "revision": "fccb8cb8810b064af161ac44c96fd2bc",
+    "url": "/car-web/static/media/i10-1.fccb8cb8.jpg"
+  },
+  {
+    "revision": "1030e0544cbeaff5b43a73683a26e7cd",
+    "url": "/car-web/static/media/i10-2.1030e054.jpg"
+  },
+  {
+    "revision": "5ab29dcc5d3add4eb1132551f8bb21dc",
+    "url": "/car-web/static/media/i10-n.5ab29dcc.JPG"
+  },
+  {
+    "revision": "9976878eede59902616057d457cb0125",
+    "url": "/car-web/static/media/i10.9976878e.jpg"
+  },
+  {
+    "revision": "33d6e7ff7a01b6acfe077b8467e88ab5",
+    "url": "/car-web/static/media/logo.33d6e7ff.PNG"
+  },
+  {
+    "revision": "0771399096bd71330dc8fa1ef576185e",
+    "url": "/car-web/static/media/maruti.07713990.JPG"
+  },
+  {
+    "revision": "093222d06a30c9730476025de913a91b",
+    "url": "/car-web/static/media/modalbg.093222d0.JPG"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/car-web/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/car-web/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/car-web/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/car-web/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
